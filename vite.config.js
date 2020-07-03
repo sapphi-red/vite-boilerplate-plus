50536e447b6c93b64e811e976add36a81759247d
sapphi-red <green@sapphi.red>
2020-07-03 19:43:16 +0900

    Fix alias

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+const path = require('path')
+
 module.exports = {
+  alias: {
+    '/@/': path.resolve(__dirname, 'src')
+  },
   proxy: {
     '/api': {
       target: 'https://example.com',
